d03f3272454b4da65d457a107376a7af577408a0
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2023-02-01 10:54:05 +0545

    register login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
-
-
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 function App() {
+  const padding = {
+    padding: 5
+  }
   return (
-   <div>
-    <h2>Hello Hi</h2>
-   </div>
+    <div className="container">
+      <h2>Book Review App</h2>
+      <Router>
+        <div>
+          <Link style={padding} to={'/'}>home</Link>
+          <Link style={padding} to={'/login'}>login</Link>
+          <Link style={padding} to={'/register'}>register</Link>
+        </div>
+
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
